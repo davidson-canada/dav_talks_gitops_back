0938cdfb6a2be7880c3bdce0f3e7740ef75078a9
davidson-canada-devops <alexandre.theve@davidson.group>
2019-10-01 17:31:32 -0400

    suppression fichier gcloud

--- server.js
+++ server.js
@@ -13,5 +13,8 @@ console.log(config.conf.env + ' API server started on: ' + port);
 
 app.get("/api/url/:name", (req, res, next) => {
     var name = req.params.name;
+
+    console.log("J'ai cru voir un micro service par ici ! ==> " + name);
+
     res.send("Coucou " + name + " from env: " +  config.conf.env);
-   });
+});
